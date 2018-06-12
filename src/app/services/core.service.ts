8daff993de67923ce4237c1e6b51604f301ecf0a
AlekseySmorschek <www.lesha007.ru@mail.ru>
2018-06-12 23:22:10 +0400

    logout

--- src/app/services/core.service.ts
+++ src/app/services/core.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 export abstract class CoreService {
-  private static  portBack ='http://localhost:8080/';
-  //private static  portBack = 'http://93.125.115.115:8080/decisions/';
+  //private static  portBack ='http://localhost:8080/';
+  private static  portBack = 'http://93.125.115.115:8080/decisions/';
   protected webService: string;
   constructor() {
     this.webService = CoreService.portBack;
